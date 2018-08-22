25b2b5880925cac36f87a2aced3706c852c7878f
TheWickerMan <jack.hall95@hotmail.co.uk>
2018-08-22 11:29:12 +0100

    Modified log module to allow for module argument

--- Modules/LoggingModule.py
+++ Modules/LoggingModule.py
@@ -1,6 +1,6 @@
 import datetime
 
 class Logging():
-    def Log(File, Level, Message):
+    def Log(File, Module, Level, Message):
         with open(File, "a") as Log:
-            Log.write("{} - {} - {}\n".format(datetime.datetime.now(), Level, Message))
+            Log.write("{} - {} - {} - {}\n".format(datetime.datetime.now(), Module, Level, Message))
